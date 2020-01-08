948cd2c1c45095dd988c8e9dea54e326ec1abe7b
trox115 <tofernandes67@gmail.com>
2020-01-08 22:20:46 +0000

    more linting issues

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = props => {
+const Display = (props) => {
   const {value} = props;
   return (<div>{value}</div>);
 };
