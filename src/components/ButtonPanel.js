9c899f456b7f0b3a0b6aa7d85f5a94d6781d69b8
trox115 <tofernandes67@gmail.com>
2020-01-08 22:14:26 +0000

    added type button for button

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,34 +3,34 @@ import React from 'react';
 const ButtonPanel = () => (
   <div className="buttons">
     <div className="group1">
-      <button name="AC" />
-      <button name="+/-" />
-      <button name="%" />
-      <button name="/" />
+      <button name="AC" type="button" />
+      <button name="+/-" type="button" />
+      <button name="%" type="button" />
+      <button name="/" type="button" />
     </div>
     <div className="group2">
-      <button name="7" />
-      <button name="8" />
-      <button name="9" />
-      <button name="X" />
+      <button name="7" type="button" />
+      <button name="8" type="button" />
+      <button name="9" type="button" />
+      <button name="X" type="button" />
     </div>
     <div className="group3">
-      <button name="4" />
-      <button name="5" />
-      <button name="6" />
-      <button name="-" />
+      <button name="4" type="button" />
+      <button name="5" type="button" />
+      <button name="6" type="button" />
+      <button name="-" type="button" />
     </div>
     <div className="group4">
-      <button name="3" />
-      <button name="2" />
-      <button name="1" />
-      <button name="+" />
+      <button name="3" type="button" />
+      <button name="2" type="button" />
+      <button name="1" type="button" />
+      <button name="+" type="button" />
     </div>
     <div className="group5">
-      <button name="0" />
-      <button name="," />
-      <button name="." />
-      <button name="=" />
+      <button name="0" type="button" />
+      <button name="," type="button" />
+      <button name="." type="button" />
+      <button name="=" type="button" />
     </div>
   </div>
 );
