00b0afb303155ebb0e95f6cefa3d3d5634134b4a
trox115 <tofernandes67@gmail.com>
2020-01-08 17:23:38 +0000

    Added Button.js Proptypes and returns a button

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+const Button = (props) =>{
+    const btname = props,
+        return(
+            <button className = 'calc'>
+        );
+}
+Button.propTypes = {
+    btname:PropTypes.string,
+}
