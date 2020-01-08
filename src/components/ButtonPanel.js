604cc38055f32a17fa456964f03e983f09e8c90d
trox115 <tofernandes67@gmail.com>
2020-01-08 23:19:24 +0000

    fixed linting on button.js hope it's the final

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,36 +1,37 @@
 import React from 'react';
+import Button from './Button';
 
 const ButtonPanel = () => (
   <div className="buttons">
     <div className="group1">
-      <button name="AC" type="button" />
-      <button name="+/-" type="button" />
-      <button name="%" type="button" />
-      <button name="/" type="button" />
+      <button btname="AC" type="button" />
+      <button btname="+/-" type="button" />
+      <button btname="%" type="button" />
+      <button btname="/" type="button" />
     </div>
     <div className="group2">
-      <button name="7" type="button" />
-      <button name="8" type="button" />
-      <button name="9" type="button" />
-      <button name="X" type="button" />
+      <button btname="7" type="button" />
+      <button btname="8" type="button" />
+      <button btname="9" type="button" />
+      <button btname="X" type="button" />
     </div>
     <div className="group3">
-      <button name="4" type="button" />
-      <button name="5" type="button" />
-      <button name="6" type="button" />
-      <button name="-" type="button" />
+      <button btname="4" type="button" />
+      <button btname="5" type="button" />
+      <button btname="6" type="button" />
+      <button btname="-" type="button" />
     </div>
     <div className="group4">
-      <button name="3" type="button" />
-      <button name="2" type="button" />
-      <button name="1" type="button" />
-      <button name="+" type="button" />
+      <button btname="3" type="button" />
+      <button btname="2" type="button" />
+      <button btname="1" type="button" />
+      <button btname="+" type="button" />
     </div>
     <div className="group5">
-      <button name="0" type="button" />
-      <button name="," type="button" />
-      <button name="." type="button" />
-      <button name="=" type="button" />
+      <button btname="0" type="button" />
+      <button btname="," type="button" />
+      <button btname="." type="button" />
+      <button btname="=" type="button" />
     </div>
   </div>
 );
