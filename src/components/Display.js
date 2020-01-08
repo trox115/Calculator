cebaf93fa12ed9015bbbed050b6c60a02100b0d8
trox115 <tofernandes67@gmail.com>
2020-01-08 22:22:13 +0000

    Fixed Display errors

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = (props) => {
+const Display = props => {
   const {value} = props;
-  return (<div>{value}</div>);
+  return <div>{value}</div>;
 };
 Display.defaultProps = {
   value: '0',
