5b120460135f7ab588a2e4ef9238b883ecc151b5
trox115 <tofernandes67@gmail.com>
2020-01-09 21:16:07 +0000

    fixed some linting errors on ButtonPannel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -7,30 +7,30 @@ const ButtonPanel = () => (
       <Button btname="AC" type="Button" />
       <Button btname="+/-" type="Button" />
       <Button btname="%" type="Button" />
-      <Button btname="/" type="Button" color='orange'/>
+      <Button btname="/" type="Button" color="orange" />
     </div>
     <div className="group2">
       <Button btname="7" type="Button" />
       <Button btname="8" type="Button" />
       <Button btname="9" type="Button" />
-      <Button btname="X" type="Button" color='orange'/>
+      <Button btname="X" type="Button" color="orange" />
     </div>
     <div className="group3">
       <Button btname="4" type="Button" />
       <Button btname="5" type="Button" />
       <Button btname="6" type="Button" />
-      <Button btname="-" type="Button" color='orange'/>
+      <Button btname="-" type="Button" color="orange" />
     </div>
     <div className="group4">
       <Button btname="3" type="Button" />
       <Button btname="2" type="Button" />
       <Button btname="1" type="Button" />
-      <Button btname="+" type="Button" color='orange'/>
+      <Button btname="+" type="Button" color="orange" />
     </div>
     <div className="group5">
-      <Button btname="0" type="Button" wide='1'/>
+      <Button btname="0" type="Button" wide="1" />
       <Button btname="." type="Button" />
-      <Button btname="=" type="Button" color='orange'/>
+      <Button btname="=" type="Button" color="orange" />
     </div>
   </div>
 );
