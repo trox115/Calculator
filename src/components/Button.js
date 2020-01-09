2e7f9278b7594e981fa0ed7517e7a03f7d804e5d
trox115 <tofernandes67@gmail.com>
2020-01-09 21:06:34 +0000

    default values for linting issues on color and wide props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = props => {
-  let cssClass = ['calc'];
-  const {btname, wide, color} = props;
+const Button = (props) => {
+  const cssClass = ['calc'];
+  const { btname, wide, color } = props;
   if (wide) {
     cssClass.push('basis50');
   }
@@ -17,6 +17,10 @@ const Button = props => {
   );
 };
 
+Button.defaultProps = {
+  color: 'gray',
+  wide: '',
+};
 Button.propTypes = {
   btname: PropTypes.string.isRequired,
   wide: PropTypes.number,
