03f9aa340a9c989e15d3036f68e0a92a468bfd8c
trox115 <tofernandes67@gmail.com>
2020-01-09 20:57:17 +0000

    fixed linter issues with Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = (props) => {
-   let cssClass=['calc'];
-  const { btname,wide,color } = props;
-    if(wide){
-        cssClass.push('basis50');
-    }
-    if(color){
-        cssClass.push(color);
-    }
-  return <button className={cssClass.join(' ')} type="button">{btname}</button>;
+const Button = props => {
+  let cssClass = ['calc'];
+  const {btname, wide, color} = props;
+  if (wide) {
+    cssClass.push('basis50');
+  }
+  if (color) {
+    cssClass.push(color);
+  }
+  return (
+    <button className={cssClass.join(' ')} type="button">
+      {btname}
+    </button>
+  );
 };
 
 Button.propTypes = {
   btname: PropTypes.string.isRequired,
+  wide: PropTypes.number,
+  color: PropTypes.string,
 };
 
 export default Button;
