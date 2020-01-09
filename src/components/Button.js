35f27131e9603f25d8a24e01f620a0c6d80bb3c2
trox115 <tofernandes67@gmail.com>
2020-01-09 20:08:48 +0000

    added 50 basis to 0

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 
 const Button = (props) => {
   const { btname } = props;
+    if (btname==='0'){
+  return <button className="calc basis50" type="button">{btname}</button>;
+    }else{
   return <button className="calc" type="button">{btname}</button>;
+    }
 };
 
 Button.propTypes = {
