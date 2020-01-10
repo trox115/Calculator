5b27f2b9f359b994053256cfb6159578168cc728
trox115 <tofernandes67@gmail.com>
2020-01-10 23:56:40 +0000

    fixed switch for linter

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,12 +1,12 @@
-let Big = require('big.js');
+const Big = require('big.js');
 
 function operate(numberOne, numberTwo, operation) {
   const num1 = new Big(numberOne);
-    const num2 = new Big(numberTwo);
+  const num2 = new Big(numberTwo);
   let result = 0;
   switch (operation) {
     case '-':
-      return num1.minus(num2);
+      result = num1.minus(num2);
       break;
 
     case '+':
@@ -15,16 +15,16 @@ function operate(numberOne, numberTwo, operation) {
       break;
     case 'X':
       result = num1.times(num2);
-      return result.toString;
     case '/':
       result = num1.div(num2);
-      return result.toString;
       break;
     case '%':
       result = num1.times(num2) / 100;
-      return result.toString;
+      break;
+    default:
+      result = 0;
       break;
   }
-  return '0';
+  return result.toString;
 }
 export default operate;
