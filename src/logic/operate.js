5f411a52cf544e4edbf73631fcf9c5712781dad8
trox115 <tofernandes67@gmail.com>
2020-01-10 23:58:05 +0000

    fixed linter issues

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -11,10 +11,10 @@ function operate(numberOne, numberTwo, operation) {
 
     case '+':
       result = num1.plus(num2);
-      return result.toString;
       break;
     case 'X':
       result = num1.times(num2);
+      break;
     case '/':
       result = num1.div(num2);
       break;
