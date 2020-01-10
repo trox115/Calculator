30409b414a602fdf9dd3dbd2bc4bf5773432f911
trox115 <tofernandes67@gmail.com>
2020-01-10 23:05:58 +0000

    changed to use big library, now I get it

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,25 +1,27 @@
 let Big = require('big.js');
 
 function operate(numberOne, numberTwo, operation) {
+  const num1 = new Big(numberOne);
+    const num2 = new Big(numberTwo);
   let result = 0;
   switch (operation) {
     case '-':
-      return numberOne - numberTwo;
+      return num1.minus(num2);
       break;
 
     case '+':
-      result = numberOne + numberTwo;
+      result = num1.plus(num2);
       return result.toString;
       break;
     case 'X':
-      result = numberOne * numberTwo;
+      result = num1.times(num2);
       return result.toString;
     case '/':
-      result = numberOne / numberTwo;
+      result = num1.div(num2);
       return result.toString;
       break;
     case '%':
-      result = (numberOne * numberTwo) / 100;
+      result = num1.times(num2) / 100;
       return result.toString;
       break;
   }
