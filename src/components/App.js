1e3509346616007e8dd4af82ff7893e5ad0a9674
trox115 <tofernandes67@gmail.com>
2020-01-11 00:33:54 +0000

    error fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-/* eslint no-unused-vars: "error" */
+/* eslint-disable no-unused-vars */
 import calculate from '../logic/calculate';
 
-function shutError() {
-  const button = 'X';
-  const obj = {
-    next: 1,
-    operation: 'X',
-    total: '1',
-  };
-  calculate(obj, button);
-  return null;
-}
 const App = () => (
-  shutError(),
-  (
-    <div id="main-div">
-      <Display />
-      <ButtonPanel />
-    </div>
-  )
+  <div id="main-div">
+    <Display />
+    <ButtonPanel />
+  </div>
 );
 
 export default App;
