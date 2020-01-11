6bf8b22434869f2d9c5b01bdd19b6e86c4fe329e
trox115 <tofernandes67@gmail.com>
2020-01-11 00:26:40 +0000

    now I think I shut the error

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,18 @@ import ButtonPanel from './ButtonPanel';
 /* eslint no-unused-vars: "error" */
 import calculate from '../logic/calculate';
 
+     function shutError(){
+        const button = 'X';
+          const obj = {
+      next:1,
+      operation: 'X',
+      total:'1',
+    };
+        calculate(obj,button);
+         return null;
+    }
 const App = () => (
+    shutError(),
   <div id="main-div">
     <Display />
     <ButtonPanel />
