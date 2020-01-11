f48bf035e06f1a4179a402eee2d13bcc54feb536
trox115 <tofernandes67@gmail.com>
2020-01-11 00:28:05 +0000

    add some formating

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,24 @@ import ButtonPanel from './ButtonPanel';
 /* eslint no-unused-vars: "error" */
 import calculate from '../logic/calculate';
 
-     function shutError(){
-        const button = 'X';
-          const obj = {
-      next:1,
-      operation: 'X',
-      total:'1',
-    };
-        calculate(obj,button);
-         return null;
-    }
+function shutError() {
+  const button = 'X';
+  const obj = {
+    next: 1,
+    operation: 'X',
+    total: '1',
+  };
+  calculate(obj, button);
+  return null;
+}
 const App = () => (
-    shutError(),
-  <div id="main-div">
-    <Display />
-    <ButtonPanel />
-  </div>
+  shutError(),
+  (
+    <div id="main-div">
+      <Display />
+      <ButtonPanel />
+    </div>
+  )
 );
 
 export default App;
