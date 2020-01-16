4f6ee2e2ed0954ada53bce98f1269d7fa0005e8b
trox115 <tofernandes67@gmail.com>
2020-01-16 19:54:21 +0000

    passed buttoon on click

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,33 +4,33 @@ import Button from './Button';
 const ButtonPanel = () => (
   <div className="Buttons">
     <div className="group1">
-      <Button btname="AC" type="Button" color="gray" />
-      <Button btname="+/-" type="Button" color="gray" />
-      <Button btname="%" type="Button" color="gray" />
-      <Button btname="/" type="Button" />
+        <Button btname="AC" type="Button" color="gray" onClick={() =>this.handle('') }/>
+      <Button btname="+/-" type="Button" color="gray" onClick={() =>this.handle('') }/>
+      <Button btname="%" type="Button" color="gray" onClick={() =>this.handle('') }/>
+      <Button btname="/" type="Button" onClick={() =>this.handle('') }/>
     </div>
     <div className="group2">
-      <Button btname="7" type="Button" color="gray" />
-      <Button btname="8" type="Button" color="gray" />
-      <Button btname="9" type="Button" color="gray" />
-      <Button btname="X" type="Button" />
+      <Button btname="7" type="Button" color="gray" onClick={() =>this.handle('') }/>
+      <Button btname="8" type="Button" color="gray" onClick={() =>this.handle('') }/>
+      <Button btname="9" type="Button" color="gray" onClick={() =>this.handle('') }/>
+      <Button btname="X" type="Button" onClick={() =>this.handle('') }/>
     </div>
     <div className="group3">
-      <Button btname="4" type="Button" color="gray" />
-      <Button btname="5" type="Button" color="gray" />
-      <Button btname="6" type="Button" color="gray" />
-      <Button btname="-" type="Button" />
+      <Button btname="4" type="Button" color="gray" onClick={() =>this.handle('') }/>
+      <Button btname="5" type="Button" color="gray" onClick={() =>this.handle('') }/>
+      <Button btname="6" type="Button" color="gray" onClick={() =>this.handle('') }/>
+      <Button btname="-" type="Button" onClick={() =>this.handle('') }/>
     </div>
     <div className="group4">
-      <Button btname="3" type="Button" color="gray" />
-      <Button btname="2" type="Button" color="gray" />
-      <Button btname="1" type="Button" color="gray" />
-      <Button btname="+" type="Button" color="orange" />
+      <Button btname="3" type="Button" color="gray" onClick={() =>this.handle('') }/>
+      <Button btname="2" type="Button" color="gray" onClick={() =>this.handle('') }/>
+      <Button btname="1" type="Button" color="gray" onClick={() =>this.handle('') }/>
+      <Button btname="+" type="Button" color="orange" onClick={() =>this.handle('') }/>
     </div>
     <div className="group5">
-      <Button btname="0" type="Button" wide="1" color="gray" />
-      <Button btname="." type="Button" color="gray" />
-      <Button btname="=" type="Button" />
+      <Button btname="0" type="Button" wide="1" color="gray" onClick={() =>this.handle('') }/>
+      <Button btname="." type="Button" color="gray" onClick={() =>this.handle('') }/>
+      <Button btname="=" type="Button" onClick={() =>this.handle('') }/>
     </div>
   </div>
 );
