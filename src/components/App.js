e56f5433d910d16913166d715026ed220da691d5
trox115 <tofernandes67@gmail.com>
2020-01-16 19:32:20 +0000

    added handle, next operation and calculate default null

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,37 @@ import ButtonPanel from './ButtonPanel';
 /* eslint-disable no-unused-vars */
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <div id="main-div">
-    <Display />
-    <ButtonPanel />
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            next: null,
+            operation: null,
+            total:null,
+        }
+    };
+    handle(btname){
+        const { next, operation, total } = calculate(this.state, btname)
+        this.setState({
+            operation,
+            total,
+            next,
+        });
+    }
+    render(){
+        let value = null;
+        if(this.state.operation == null){
+            value = this.state.total ? this.state.total : '0';
+        }else{
+            value = this.state.next ? this.state.next : this.state.total;
+        }
+        return (
+            <div id="main-div">
+         <Display value = { value } />
+         <ButtonPanel onClick={btname =>this.handleClick(btname)} />
   </div>
-);
+)
+    }
+}
 
 export default App;
