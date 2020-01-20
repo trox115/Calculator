28c32df507776c7d7dbc95f32890a5ad3109bb8f
trox115 <tofernandes67@gmail.com>
2020-01-20 23:51:09 +0000

    fixed no else after return

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,4 +1,4 @@
-let Big = require('big.js');
+const Big = require('big.js');
 
 const operate = (numberOne, numberTwo, operation) => {
   const num1 = Big(numberOne);
@@ -8,26 +8,26 @@ const operate = (numberOne, numberTwo, operation) => {
     result = 'Cannot divide by 0';
     return result;
   }
-    switch (operation) {
-      case '-':
-        result = num1.minus(num2);
-        break;
-      case '+':
-        result = num1.plus(num2);
-        break;
-      case 'X':
-        result = num1.times(num2);
-        break;
-      case '/':
-        result = num1.div(num2);
-        break;
-      case '%':
-        result = num1.times(num2) / 100;
-        break;
-      default:
-        result = 0;
-        break;
-    }
+  switch (operation) {
+    case '-':
+      result = num1.minus(num2);
+      break;
+    case '+':
+      result = num1.plus(num2);
+      break;
+    case 'X':
+      result = num1.times(num2);
+      break;
+    case '/':
+      result = num1.div(num2);
+      break;
+    case '%':
+      result = num1.times(num2) / 100;
+      break;
+    default:
+      result = 0;
+      break;
+  }
   return result.toString();
 };
 
