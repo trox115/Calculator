df520ed51acd79b778d839adb675b0bc2ac38961
trox115 <tofernandes67@gmail.com>
2020-01-20 23:25:10 +0000

    fixed calculate.js some linting errors

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,24 +1,24 @@
 import operate from './operate';
 
 const calculate = (obj, btname) => {
-  const { next, operation } = obj;
-  let { total } = obj;
+  const {next, operation} = obj;
+  let {total} = obj;
 
-      console.log(total +"total")
-      console.log(next +" next")
+  console.log(total + 'total');
+  console.log(next + ' next');
   if (btname === '+/-') {
     total *= -1;
     obj.total = total;
   } else {
     obj.total = operate(total, next, operation);
-      obj.operation= operation
+    obj.operation = operation;
   }
   return obj;
 };
 
 const isNumber = (calculator, btname) => {
-  let { total, next } = calculator;
-  const { operation } = calculator;
+  let {total, next} = calculator;
+  const {operation} = calculator;
   if (operation === null) {
     if (total === null) {
       total = btname;
@@ -30,20 +30,20 @@ const isNumber = (calculator, btname) => {
   } else {
     next += btname;
   }
-  return { next, operation, total };
+  return {next, operation, total};
 };
 
-const isOperator= (calculator, btname) => {
-  let { next, operation, total } = calculator;
+const isOperator = (calculator, btname) => {
+  let {next, operation, total} = calculator;
   if (operation === null || next === null) {
     if (btname === '+/-') {
-      const { total: calcTotal } =calculate (calculator, btname);
+      const {total: calcTotal} = calculate(calculator, btname);
       total = calcTotal;
     } else {
       operation = btname;
     }
   } else {
-    const { total: calcTotal } =calculate(calculator, btname);
+    const {total: calcTotal} = calculate(calculator, btname);
     if (btname === '=') {
       next = null;
       total = calcTotal;
@@ -53,20 +53,19 @@ const isOperator= (calculator, btname) => {
       total = calcTotal;
     }
   }
-  return { next, operation, total };
+  return {next, operation, total};
 };
 const main = (calculator, btname) => {
-  let { next, operation, total } = calculator;
+  let {next, operation, total} = calculator;
 
   if (btname === 'AC' || total === 'Cannot divide by 0') {
-    ({ next, operation, total } = { next: null, operation: null, total: null });
+    ({next, operation, total} = {next: null, operation: null, total: null});
   } else if (!isNaN(btname)) {
-    ({ next, operation, total } = isNumber(calculator, btname));
+    ({next, operation, total} = isNumber(calculator, btname));
   } else {
-    ({ next, operation, total } = isOperator(calculator,btname));
+    ({next, operation, total} = isOperator(calculator, btname));
   }
 
-  return { next, operation, total };
+  return {next, operation, total};
 };
 export default main;
-
