371977b95a6bbae5a9c4f938d82137cfed0222c8
trox115 <tofernandes67@gmail.com>
2020-01-20 22:26:26 +0000

    fixed ternary conditional

--- src/components/App.js
+++ src/components/App.js
@@ -27,9 +27,9 @@ class App extends React.Component {
     const { next, operation, total } = this.state;
     let value = null;
     if (operation === null) {
-      value = total ? total : '0';
+      value = total || '0';
     } else {
-      value = next ? next : total;
+      value = next || total;
     }
     return (
       <div id="main-div">
