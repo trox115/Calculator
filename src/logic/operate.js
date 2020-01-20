dc32abcda16be398ba69e2ef2cf38ba550573566
trox115 <tofernandes67@gmail.com>
2020-01-20 23:48:51 +0000

    more operate indentation problems

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,36 +1,35 @@
 let Big = require('big.js');
 
 const operate = (numberOne, numberTwo, operation) => {
-  let num1 = Big(numberOne);
-  let num2 = Big(numberTwo);
+  const num1 = Big(numberOne);
+  const num2 = Big(numberTwo);
   let result = 0;
-  if (operation === '/' && numberTwo ==='0' ) {
-    result = 'Cannot divide by 0'
+  if (operation === '/' && numberTwo === '0') {
+    result = 'Cannot divide by 0';
     return result;
   } else {
-
-  switch (operation) {
-    case '-':
-      result = num1.minus(num2);
-      break;
-    case '+':
-      result = num1.plus(num2);
-      break;
-    case 'X':
-      result = num1.times(num2);
-      break;
-    case '/':
+    switch (operation) {
+      case '-':
+        result = num1.minus(num2);
+        break;
+      case '+':
+        result = num1.plus(num2);
+        break;
+      case 'X':
+        result = num1.times(num2);
+        break;
+      case '/':
         result = num1.div(num2);
-      break;
-    case '%':
-      result = num1.times(num2) / 100;
-      break;
-    default:
-      result = 0;
-      break;
-   }
+        break;
+      case '%':
+        result = num1.times(num2) / 100;
+        break;
+      default:
+        result = 0;
+        break;
+    }
   }
   return result.toString();
-}
+};
 
 export default operate;
