d375cdfd42b309f4d9700d30b69074e81e9348eb
trox115 <tofernandes67@gmail.com>
2020-01-20 20:27:06 +0000

    fixed App.js linter problems

--- src/components/App.js
+++ src/components/App.js
@@ -5,37 +5,37 @@ import ButtonPanel from './ButtonPanel';
 import main from '../logic/calculate';
 
 class App extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            next: null,
-            operation: null,
-            total:null,
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      next: null,
+      operation: null,
+      total: null,
     };
-    handle(btname){
-        const { next, operation, total } =main(this.state, btname)
-        this.setState({
-            operation,
-            total,
-            next,
-        });
-        console.log("heu")
+  }
+  handle(btname) {
+    const {next, operation, total} = main(this.state, btname);
+    this.setState({
+      operation,
+      total,
+      next,
+    });
+    console.log('heu');
+  }
+  render() {
+    let value = null;
+    if (this.state.operation == null) {
+      value = this.state.total ? this.state.total : '0';
+    } else {
+      value = this.state.next ? this.state.next : this.state.total;
     }
-    render(){
-        let value = null;
-        if(this.state.operation == null){
-            value = this.state.total ? this.state.total : '0';
-        }else{
-            value = this.state.next ? this.state.next : this.state.total;
-        }
-        return (
-            <div id="main-div">
-         <Display value = { value.toString() } />
-         <ButtonPanel onClick={btname =>this.handle(btname) } />
-  </div>
-)
-    }
+    return (
+      <div id="main-div">
+        <Display value={value.toString()} />
+        <ButtonPanel onClick={btname => this.handle(btname)} />
+      </div>
+    );
+  }
 }
 
 export default App;
