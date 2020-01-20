96248a642ffaee309a7425a59d18c50cf92bf78d
trox115 <tofernandes67@gmail.com>
2020-01-20 23:47:44 +0000

    fixed operate problems

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,22 +1,18 @@
-var Big = require('big.js');
+let Big = require('big.js');
 
 const operate = (numberOne, numberTwo, operation) => {
-    console.log(numberOne+" numero")
-    console.log(numberTwo+" numero 2")
   let num1 = Big(numberOne);
   let num2 = Big(numberTwo);
   let result = 0;
-    if (operation === '/' && numberTwo ==='0' ){
-
+  if (operation === '/' && numberTwo ==='0' ) {
     result = 'Cannot divide by 0'
-        return result;
-    } else {
+    return result;
+  } else {
 
   switch (operation) {
     case '-':
       result = num1.minus(num2);
       break;
-
     case '+':
       result = num1.plus(num2);
       break;
@@ -32,8 +28,9 @@ const operate = (numberOne, numberTwo, operation) => {
     default:
       result = 0;
       break;
+   }
   }
-    }
   return result.toString();
 }
+
 export default operate;
