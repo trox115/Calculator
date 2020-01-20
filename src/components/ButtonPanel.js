49b92c1e4c55cedc4c08d026e9d918e051069f61
trox115 <tofernandes67@gmail.com>
2020-01-20 23:15:50 +0000

    trying to fix it

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import Button from './Button';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 class ButtonPanel extends React.Component {
- static propTypes = {
-   btname: PropTypes.string,
- };
-
   handle(btname) {
     const prps = this.props;
     prps.onClick(btname);
@@ -124,5 +120,9 @@ class ButtonPanel extends React.Component {
     );
   }
 }
+
+ButtonPanel.propTypes = {
+  btname: PropTypes.string,
+};
 
 export default ButtonPanel;
