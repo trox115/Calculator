eea44e531f2ad387a04edb4b0acf30e23e84a0b3
trox115 <tofernandes67@gmail.com>
2020-01-22 22:04:16 +0000

    imported proptype on button pannel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import Button from './Button';
 
 export default class ButtonPanel extends React.PureComponent {
@@ -131,3 +132,7 @@ export default class ButtonPanel extends React.PureComponent {
     );
   }
 }
+
+Button.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
